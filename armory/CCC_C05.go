95c9f56a28d336563a42324569ddce2fb7ba7971
Sarah Craddock <sarahcra@microsoft.com>
2024-11-04 16:02:46 +0000

    Moved incomplete strike back and added allowed IPs value

--- armory/CCC_C05.go
+++ armory/CCC_C05.go
@@ -42,13 +42,32 @@ func CCC_C05_TR01_T01() (result raidengine.MovementResult) {
 		result.Passed = true
 		result.Message = "Public network access is disabled for the storage account."
 	} else if *storageAccountResource.Properties.PublicNetworkAccess == "Enabled" {
+
 		if *storageAccountResource.Properties.NetworkRuleSet.DefaultAction == "Deny" {
+
+			type AllowedIps struct {
+				Name string
+				IPs  []string
+			}
+
+			allowedIps := AllowedIps{
+				Name: "Allowlisted IPs and IP ranges",
+				IPs:  []string{},
+			}
+
+			for _, ip := range storageAccountResource.Properties.NetworkRuleSet.IPRules {
+				allowedIps.IPs = append(allowedIps.IPs, *ip.IPAddressOrRange)
+			}
+
+			result.Value = allowedIps
 			result.Passed = true
-			result.Message = "Public network access is enabled for the storage account, but the default action is set to deny for sources outside of the allowlist."
+			result.Message = "Public network access is enabled for the storage account, but the default action is set to deny for sources outside of the allowlist IPs (see result value)."
+
 		} else {
 			result.Passed = false
 			result.Message = "Public network access is enabled for the storage account and the default action is not set to deny for sources outside of the allowlist."
 		}
+
 	} else if *storageAccountResource.Properties.PublicNetworkAccess == "SecuredByPerimeter" {
 		// This isn't publicly available yet so we shouldn't hit this condition with customers
 		result.Passed = false
@@ -58,36 +77,6 @@ func CCC_C05_TR01_T01() (result raidengine.MovementResult) {
 		result.Message = fmt.Sprintf("Public network access status of %s unclear.", *storageAccountResource.Properties.PublicNetworkAccess)
 	}
 
-	return
-}
-
-// -----
-// Strike and Movements for CCC_C05_TR02
-// -----
-
-func (a *ABS) CCC_C05_TR02() (strikeName string, result raidengine.StrikeResult) {
-	strikeName = "CCC_C05_TR02"
-	result = raidengine.StrikeResult{
-		Passed:      false,
-		Description: "The service logs all access attempts from untrusted entities, including failed connection attempts.",
-		Message:     "Strike has not yet started.",
-		DocsURL:     "https://maintainer.com/docs/raids/ABS",
-		ControlID:   "CCC.C05",
-		Movements:   make(map[string]raidengine.MovementResult),
-	}
-
-	raidengine.ExecuteMovement(&result, CCC_C05_TR02_T01)
-
-	return
-}
-
-func CCC_C05_TR02_T01() (result raidengine.MovementResult) {
-	result = raidengine.MovementResult{
-		Description: "Confirms the service is configured to log all access attempts from untrusted entities, including failed connection attempts.",
-		Function:    utils.CallerPath(0),
-	}
-
-	// TODO: Use this section to write a single step or test that contributes to CCC_C05_TR02
 	return
 }
 
