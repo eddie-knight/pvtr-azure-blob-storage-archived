eeea6b68f5ebdb51135aca427d65191683e6b3d5
Eddie Knight <knight@linux.com>
2025-02-13 08:54:32 -0600

    chore: Removed cmd/ as part of SDK upgrade
    Signed-off-by: Eddie Knight <knight@linux.com>

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/azure/finos-azure-blob-storage-raid/cmd"
+	"os"
+
+	abs "github.com/azure/finos-azure-blob-storage-raid/ABS"
+
+	"github.com/privateerproj/privateer-sdk/command"
+	"github.com/privateerproj/privateer-sdk/config"
 )
 
 var (
@@ -15,11 +20,36 @@ var (
 	GitCommitHash = ""
 	// BuiltAt is the actual build datetime
 	BuiltAt = ""
+
+	PluginName   = "github-repo"
+	RequiredVars = []string{
+		"storageAccountResourceId",
+		"allowedRegions",
+	}
+
+	runCmd = command.NewPluginCommands(
+		PluginName,
+		Version,
+		VersionPostfix,
+		GitCommitHash,
+		&abs.Armory,
+		initializer,
+		RequiredVars,
+	)
 )
+
+// initializer is a custom function to set up the armory for our usecase
+func initializer(c *config.Config) (err error) {
+	return abs.Initialize()
+}
 
 func main() {
 	if VersionPostfix != "" {
 		Version = fmt.Sprintf("%s-%s", Version, VersionPostfix)
 	}
-	cmd.Execute(Version, GitCommitHash, BuiltAt)
+
+	err := runCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
